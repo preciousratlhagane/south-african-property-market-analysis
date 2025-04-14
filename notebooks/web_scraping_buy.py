ff1a9000dd5e095755094ef893e86a03decaf301
Precious Ratlhagane <precious_555@live.com>
2025-04-14 20:15:24 +0200

    Add imports for the web scraping script

---
+++ notebooks/web_scraping_buy.py
@@ -0,0 +1,34 @@
+import csv
+import os
+import random
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+# Load enviromental_variables:
+load_dotenv()
+base_listing_url_for_rent = os.getenv("BASE_LISTING_URL_FOR_SALE")
+
+# Generate a list of user agents
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0) Gecko/20100101 Firefox/124.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0"
+]
+
+# Create a function to mimic human_like behaviour when scrolling on a website
+
+
+def human_like_delay():
+    base_delay = random.uniform(3, 10)
+    jitter = random.uniform(-1, 2)
+    time.sleep(base_delay + jitter)
